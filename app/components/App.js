71386297379c0e8318904d71602471d23127464d
David Goldberg <drgold1988@gmail.com>
2015-07-22 10:38:27 -0700

    [feat]: creates controller-view component listens for changes in TodoStore

---
+++ app/components/App.js
@@ -0,0 +1,44 @@
+var Footer = require('./Footer');
+var Header = require('./Header');
+var MainSection = require('./MainSection');
+var React = require('react');
+var TodoStore = require('../stores/TodoStore');
+
+// Retrieve current TODO data from TodoStore
+function getTodoState() {
+  return {
+    allTodos: TodoStore.getAll(),
+    areAllComplete: TodoStore.areAllComplete();
+  };
+}
+
+var TodoApp = React.createClass({
+  getInitialState: function() {
+    return getTodoState();
+  },
+  componentDidMount: function() {
+    TodoStore.addChangeListener(this._onChange);
+  },
+  componentWillUnmount: function() {
+    TodoStore.removeChangeListener(this._onChange);
+  },
+  render: function() {
+    return (
+      <div>
+        <Header />
+        <MainSection
+          allTodos={this.state.allTodos}
+          areAllComplete={this.state.areAllComplete} />
+        <Footer allTodos={this.state.allTodos} />
+      </div>
+    );
+  },
+
+  // Event handler for change events coming from TodoStore
+  _onChange: function() {
+    this.setState(getTodoState());
+  }
+
+});
+
+module.exports = TodoApp;
